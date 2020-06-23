278eefedb19025f99983b3d59628fd2e3a1888d8
Tristan Guest <tristanguest@gmail.com>
2020-06-23 14:56:47 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,46 @@
 //define functions here
 
+function getIt() {
+  $("p").on('click', function(){
+    alert("Hey!")
+    return
+  })
+}
+
+function frameIt() {
+  
+  $('img').on('load', function(){
+      $('img')[0].classList.add('tasty')
+    // alert('tasty!')
+    return
+  });
+}
+
+function pressIt() {
+  $('#typing').on("keydown", 
+  function(key) {
+    if(key.which == 71) {
+      alert('g was pressed')
+    }
+    return
+  })
+}
+
+function submitIt() {
+  
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.")
+    return
+  })
+}
+
+
 $(document).ready(function(){
 
-// call functions here
+  // call functions here
+  getIt()
+  frameIt()
+  pressIt()
+  submitIt()
 
 });
